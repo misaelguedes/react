4ac264975e8805d7417162f33554e908ff816903
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-08-08 15:17:08 -0300

    Entendendo o JSX

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,14 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nome = 'Misael'
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Olá React</h1>
-        <p>
-          Meu primeiro app.
-        </p>
+        <h2>Meu Primeiro App</h2>
+        <p>Olá, {nome}</p>
+        <img src={url} alt="Minha Imagem"/>
         <a
           className="App-link"
           href="https://reactjs.org"
