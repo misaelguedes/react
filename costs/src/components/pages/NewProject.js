239e7a00a57b35cd130eab14ee6140373e6434d6
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-08-16 15:19:14 -0300

    Mensagens do Sistema

--- costs/src/components/pages/NewProject.js
+++ costs/src/components/pages/NewProject.js
@@ -23,7 +23,7 @@ export default function NewProject() {
         .then((data) => {
             console.log(data)
             // redirect
-            navigate('/projects', {mensagem: 'Projeto criado com sucesso!'})
+            navigate('/projects', {state: {message: 'Projeto criado com sucesso!'}})
         })
         .catch((err) => console.log(err))
     }
